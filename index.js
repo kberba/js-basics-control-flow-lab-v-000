b07eb947f6737e6ad7574850ede853bbcd072e63
Karen Berba <kberba@gmail.com>
2018-01-31 04:50:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Write your code in this file!
 
-var ride_distance;
-
-function scuberGreetingForFeet(ride_distance) {
-  if (ride_distance <= 400) {
-    console.log("This one is on me!");
-  } else if (ride_distance > 2000 && ride_distance < 2500) {
-    console.log("I will gladly take your thirty bucks.");
-  }
+function scuberGreetingForFeet() {
+  
 }
+
+// var ride_distance;
+
+// function scuberGreetingForFeet(ride_distance) {
+//   if (ride_distance <= 400) {
+//     console.log("This one is on me!");
+//   } else if (ride_distance > 2000 && ride_distance < 2500) {
+//     console.log("I will gladly take your thirty bucks.");
+//   }
+// }
