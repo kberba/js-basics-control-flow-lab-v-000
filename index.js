ddf2c1264dad79c97c184a58e9ddaa39269cbc7c
Karen Berba <kberba@gmail.com>
2018-01-31 04:34:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
 
-let scuberGreetingForFeet(ride_distance) {
-  // if (ride_distance <= 400) {
-  //   print("This one is one me!");
-  // }
+scuberGreetingForFeet(ride_distance) {
+  if (ride_distance <= 400) {
+    print("This one is one me!");
+  }
 
 };
