9f674310cfd91bdf824c97cc6ca614159b67747e
Karen Berba <kberba@gmail.com>
2018-01-31 04:56:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 function scuberGreetingForFeet(rd) {
   if (rd <= 400) {
     return "This one is on me!";
-  }
+  } else if (ride_distance > 2000 && ride_distance < 2500) {
+      return "I will gladly take your thirty bucks.";
+    }
 }
 
 function ternaryCheckCity() {
@@ -11,15 +13,7 @@ function ternaryCheckCity() {
 }
 
 function switchOnCharmFromTip() {
-  
+
 }
 
-// var ride_distance;
 
-// function scuberGreetingForFeet(ride_distance) {
-//   if (ride_distance <= 400) {
-//     console.log("This one is on me!");
-//   } else if (ride_distance > 2000 && ride_distance < 2500) {
-//     console.log("I will gladly take your thirty bucks.");
-//   }
-// }
