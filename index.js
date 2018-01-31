71a3a8c5e32bf2c1ccadb210c8acc800a32445b7
Karen Berba <kberba@gmail.com>
2018-01-31 04:44:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 function scuberGreetingForFeet(ride_distance) {
   if (ride_distance <= 400) {
-    print("This one is on me!");
-  };
+    console.log("This one is on me!");
+  } else if (ride_distance > 2000 && ride_distance < 2500) {
+    console.log("I will gladly take your thirty bucks.");
+  }
 
 };
