0128ea609595a2aaed16746a23ce26a87ce60315
Karen Berba <kberba@gmail.com>
2018-01-31 04:40:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 
-scuberGreetingForFeet(ride_distance) {
+function scuberGreetingForFeet(ride_distance) {
   if (ride_distance <= 400) {
     print("This one is one me!");
   }
